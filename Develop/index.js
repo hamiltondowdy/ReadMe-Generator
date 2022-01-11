ad228317f6363e2c06f3cef560ec02310a6e7f7f
hamiltondowdy <92259496+hamiltondowdy@users.noreply.github.com>
2022-01-10 19:16:51 -0600

    added questions for user input

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,41 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'provide a project title (required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('provide title');
+                return false;
+            }
+        }  
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'enter github username (required)',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('enter github username');
+                return false;
+            }
+        }
+    },
+{
+
+}
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
