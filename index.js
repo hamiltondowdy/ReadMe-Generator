8fb32949ec98c7a4a5dfe291b870b945c86d8aed
hamiltondowdy <92259496+hamiltondowdy@users.noreply.github.com>
2022-01-16 16:03:57 -0600

    updated markdown to include features

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-// Packages needed for this application
-const fs = require('fs');
-const inquirer = require('inquirer');
+// require modules 
+const fs = require('fs'); 
+const inquirer = require('inquirer'); 
+
+// linking to page where the README is developed 
 const generateMarkdown = require('./utils/generateMarkdown.js');
-const util = require('util');
+
 
 // Array of questions for user input
 const questions = [
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,106 +1,34 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  if (license) {
-    return `![${license} License] (https://choosealicense.com/badge/license-${license.split(' ').join('%20')}-blue)`;
-  } else {
-    return '';
-  }
-}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {
-  if (license) {
-    return `![${license} License] (https://choosealicense.com/license-${license})`;
-  } else {
-    return '';
-  }
-}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-    if (license !== 'no license') {
-    return `
-    ## [License](#table-of-contents)
-    the application is covered under the following license:
-    ${renderLicenseLink(license)}
-      `;
-    } else {
-      return ' ';
-    }
-}
 
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `
+const generateMarkdown = data => {
+  return `# ${data.title}
+  ![Github licence](http://img.shields.io/badge/license-${data.license}-blue.svg)
   
-  # ${data.title}
+  ## Description 
   ${data.description}
-  ${data.licenseBadge}
-  [A deployed version can be viewed here.](${data.URL})
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
   
----
-  ## Contents
-  1. [About](#about)
-      1. [User Story](#user%20story)
-      2. [Acceptance criteria](#acceptance%20criteria)
-      3. [Visuals](#visuals)
-      4. [Build](#build)
-  2. [Installation](#installation)
-  3. [License](#license)
-  4. [Contributing](#contributing)
-  5. [Tests](#tests)
-  6. [Authors and acknowledgment](#authors%20and%20acknowledgment)
----
-  ## About
-  ${data.about}
----
-  ### User Story
-  
----
-  ### Acceptance Criteria
-  
-  
----
-  ## Visuals:
-  ![]()
----
-  ## Installation:
-  ${data.installation}
-  To clone the repo:
-  
-      git clone ${data.clone}
-  
----
-  ## License
-  License used for this project - ${data.license}
-  * For more information on license types, please reference this website
-  for additional licensing information - [https: //choosealicense.com/](https://choosealicense.com/).
----
-  ## Contributing:
-  
-  To contribute to this application, create a pull request.
-  Here are the steps needed for doing that:
-  - Fork the repo
-  - Create a feature branch (git checkout -b NAME-HERE)
-  - Commit your new feature (git commit -m 'Add some feature')
-  - Push your branch (git push)
-  - Create a new Pull Request
-  Following a code review, your feature will be merged.
----
-  ## Tests:
+  ## Installation 
+  ${data.install}
+  ## Usage 
+  ${data.usage}
+  ## License 
+  This project is license under ${data.license}
+  ## Contributing 
+  ${data.contributors}
+  ## Tests
   ${data.test}
----
-  ## Authors and Acknowledgments
-  ${data.author}
----
-  ### Contact Information:
-    *GitHub Username: ${data.userName}
-    *GitHub Email: ${data.userEmail}
-  
+  ## Questions
+  If you have any questions about this projects, please contact me directly at ${data.email}. You can view more of my projects at https://github.com/${data.github}.
 `;
 }
 
+
+// use for importing Markdown in index 
 module.exports = generateMarkdown;
